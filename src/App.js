d49990f3887097451270e1bcd88ec57353389cda
bnparham <parhambn.workmail@gmail.com>
2023-11-27 10:49:12 +0330

    add routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BasicGrid from './components/Pages/MenuPage/MenuPageMain';
 import AppLayout from './AppLayout';
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
 
 import './style.css'
 import CrimePageMain from './components/Pages/CrimePage/CrimePageMain';
@@ -9,10 +10,15 @@ import CityPageMain from './components/Pages/CityPage/CityPageMain';
 
 export default function App() {
   return (
-    <AppLayout>
-      {/* <MenuPageMain/> */}
-      {/* <CrimePageMain/> */}
-      <CityPageMain />
-    </AppLayout>
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path='/' element={<MenuPageMain/>}>
+            <Route path='/crimePage' element={<CrimePageMain/>}/>
+            <Route path='/cityPage' element={<CityPageMain />}/>
+          </Route>
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
   )
 }
